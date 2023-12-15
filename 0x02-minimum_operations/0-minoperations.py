25bb234388f900525b22352f976416e5bd5f0443
Nwaigba1234567 <iamkatty3@gmail.com>
2023-12-14 17:04:30 -0800

    Added a new file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Main File for testing
+"""
+def minOperations(n):
+    """An algorithm that check a single character in a text file
+
+    Arguments:
+    =========
+    file (list of file): represent a single text file
+
+    Returns (interger): True if n is impossible to achieve
+    """
+    if n == 1:
+        return 0
+
+    # Initialize an array to store the minimum operations
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+
+    for i in range(2, n + 1):
+        # Check all possible factors of i
+        for j in range(2, i + 1):
+            if i % j == 0:
+                # Update the minimum operations
+                dp[i] = min(dp[i], dp[j] + i // j)
+    return dp[n] if dp[n] != float('inf') else 0
+# Example usage
+    n = 9
+    result = minOperations(n)
+    print(f"Number of operations for {n} H characters: {result}")
